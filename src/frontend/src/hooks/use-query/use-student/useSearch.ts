ea4a5b94056c864a56ee995c36d396759489f00b
khoinguyen26 <khoinguyen261101@gmail.com>
2022-08-19 14:05:55 +0700

    Created student list

---
+++ src/frontend/src/hooks/use-query/use-student/useSearch.ts
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+
+
+
+export default function useSearch() {
+    const [searchKey, setSearchKey] = useState('');
+
+    const searchStudentsQuery = useQuery({
+        queryKey: ['search'],
+        queryFn: async () =>
+            axios({
+                method: "GET",
+                url: "https://api.baserow.io/api/database/rows/table/91163/?user_field_names=true",
+                headers: {
+                    Authorization: "Token XBbPCgV2nXKQ5K2r6fdNnkTiYbjDw6Xf"
+                },
+                params: {
+                    ...(searchKey ? { search: searchKey, size: 5 } : {}),
+                }
+            }),
+        select: (data) => {
+            const { data: searchRes } = data;
+            return searchRes
+        },
+        onSuccess: (data) => {
+            setSearchKey(prev => prev !== null ? prev : null);
+        }
+    });
+
+    return {
+        searchStudentsQuery,
+        state: {
+            search: { searchKey, setSearchKey }
+        }
+    }
+}

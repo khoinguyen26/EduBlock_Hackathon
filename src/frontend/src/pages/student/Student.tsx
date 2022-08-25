f94dd0aabde35b1d2209e81a9602b09712be74c8
khoinguyen26 <khoinguyen261101@gmail.com>
2022-08-25 22:06:26 +0700

    create student + teacher list

--- src/frontend/src/pages/student/Student.tsx
+++ src/frontend/src/pages/student/Student.tsx
@@ -1,13 +1,125 @@
-import { Box, Typography } from '@mui/material'
+import { Table } from '@fe/components'
+import { useStudentQuery } from '@fe/hooks/use-query'
+import { Box, Stack, TextField, Typography } from '@mui/material'
+import { GridColDef, GridRowsProp } from '@mui/x-data-grid'
+import { useEffect, useState } from 'react'
 
-const TEXT = {
-  STUDENT: 'Student'
-}
+
+
+const columns: GridColDef[] = [
+  {
+    field: 'principalId',
+    headerName: 'PrincipalId',
+    editable: false
+  },
+  {
+    field: 'firstName',
+    headerName: 'FirstName',
+    editable: false
+  },
+  {
+    field: 'lastName',
+    headerName: 'LastName'
+  },
+  {
+    field: 'dob',
+    headerName: 'DateOfBirth'
+  },
+  {
+    field: 'address',
+    headerName: 'Address'
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone'
+  },
+  {
+    field: 'email',
+    headerName: 'Email'
+  },
+  {
+    field: 'gender',
+    headerName: 'Gender',
+  }
+]
+
 
 export function Student() {
+  const [page, setPage] = useState(0)
+  const [pageSize, setPageSize] = useState(5)
+
+  const { queryResult,
+
+    state: {
+      apiData: {
+        students: { students },
+      },
+      queryOptions: {
+        search: { setSearch, search }
+      } }
+  } = useStudentQuery({
+    initialProps: {
+      page: page,
+      pageSize: pageSize,
+      search: ''
+    },
+  })
+
+  const rows: GridRowsProp = students.map((_, rowIndex) => ({
+    id: _.id,
+    ...columns.reduce((prev, curr) => {
+      return {
+        ...prev,
+        [curr.field]: curr.field == "Gender" ? _.Gender.value : _[curr.field]
+      }
+    }, {})
+  })) || [];
+
+
+  useEffect(() => {
+    queryResult.refetch()
+  }, [search])
+
   return (
-    <Box>
-      <Typography>{TEXT.STUDENT}</Typography>
-    </Box>
+    <Stack height={'100%'}>
+      <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }} padding={1}>
+        <TextField
+          id="search-teacher"
+          variant='outlined'
+          label="Teacher Name"
+          value={search}
+          onChange={(e) => { setSearch(e.target.value); setPage(0) }}
+          fullWidth={false}
+          sx={{ width: '30%' }} />
+      </Stack>
+      <Table
+        columns={columns}
+        rows={rows}
+        loading={false}
+        rowCount={rows?.length}
+        page={{
+          page: page,
+          setPage(page) {
+            setPage(page)
+          }
+        }}
+        pageSize={{
+          pageSize: pageSize,
+          setPageSize(size) {
+            setPageSize(size)
+          }
+        }}
+        sort={{
+          onSortModelChange(gridSortModel) {
+            // set query here
+          }
+        }}
+        filter={{
+          onFilterModelChange(gridFilterModel) {
+            // set query here
+          }
+        }}
+      />
+    </Stack>
   )
 }
--- src/frontend/src/pages/teacher/Teacher.tsx
+++ src/frontend/src/pages/teacher/Teacher.tsx
@@ -1,32 +1,135 @@
+import { Table } from '@fe/components'
 import { useTeacherQuery } from '@fe/hooks/use-query'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import { GridColDef, GridRowsProp } from '@mui/x-data-grid'
 import { useEffect, useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
-const TEXT = {
-  TEACHER: 'TEACHER'
-}
+
+const columns: GridColDef[] = [
+  {
+    field: 'principalId',
+    headerName: 'PrincipalId',
+    editable: false
+  },
+  {
+    field: 'firstName',
+    headerName: 'FirstName',
+    editable: false
+  },
+  {
+    field: 'lastName',
+    headerName: 'LastName'
+  },
+  {
+    field: 'dob',
+    headerName: 'DateOfBirth'
+  },
+  {
+    field: 'address',
+    headerName: 'Address'
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone'
+  },
+  {
+    field: 'email',
+    headerName: 'Email'
+  },
+  {
+    field: 'gender',
+    headerName: 'Gender',
+  }
+]
 
 export function Teacher() {
   const [page, setPage] = useState(0)
   const [pageSize, setPageSize] = useState(5)
-  const [search, setSearch] = useState('')
 
-  const { queryResult } = useTeacherQuery({
+
+  const { queryResult,
+
+    state: {
+      apiData: {
+        teachers: { teachers },
+      },
+      queryOptions: {
+        search: { setSearch, search }
+      } }
+  } = useTeacherQuery({
     initialProps: {
       page: page,
       pageSize: pageSize,
+      search: ''
     },
   })
 
+  const rows: GridRowsProp = teachers.map((_, rowIndex) => ({
+    id: _.id,
+    ...columns.reduce((prev, curr) => {
+      return {
+        ...prev,
+        [curr.field]: curr.field == "Gender" ? _.Gender.value : _[curr.field]
+      }
+    }, {})
+  })) || [];
+
+
+
   useEffect(() => {
     queryResult.refetch()
-  }, [])
+  }, [search])
 
-  console.log(queryResult.data)
+
+
+
+
+
 
   return (
-    <Box>
-      <Typography>{TEXT.TEACHER}</Typography>
-    </Box>
+
+    <Stack height={'100%'}>
+      <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }} padding={1}>
+        <TextField
+          id="search-teacher"
+          variant='outlined'
+          label="Teacher Name"
+          value={search}
+          onChange={(e) => { setSearch(e.target.value); setPage(0) }}
+          fullWidth={false}
+          sx={{ width: '30%' }} />
+      </Stack>
+      <Table
+        columns={columns}
+        rows={rows}
+        loading={false}
+        rowCount={rows?.length}
+        page={{
+          page: page,
+          setPage(page) {
+            setPage(page)
+          }
+        }}
+        pageSize={{
+          pageSize: pageSize,
+          setPageSize(size) {
+            setPageSize(size)
+          }
+        }}
+        sort={{
+          onSortModelChange(gridSortModel) {
+            // set query here
+          }
+        }}
+        filter={{
+          onFilterModelChange(gridFilterModel) {
+            // set query here
+          }
+        }}
+      />
+    </Stack>
   )
+
 }
+

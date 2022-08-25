95c890e69670dbe9b45863b2d8220334c49649ae
khoinguyen26 <khoinguyen261101@gmail.com>
2022-08-25 10:56:19 +0700

    add teacher query

--- src/frontend/src/pages/teacher/Teacher.tsx
+++ src/frontend/src/pages/teacher/Teacher.tsx
@@ -1,10 +1,29 @@
+import { useTeacherQuery } from '@fe/hooks/use-query'
 import { Box, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 const TEXT = {
   TEACHER: 'TEACHER'
 }
 
 export function Teacher() {
+  const [page, setPage] = useState(0)
+  const [pageSize, setPageSize] = useState(5)
+  const [search, setSearch] = useState('')
+
+  const { queryResult } = useTeacherQuery({
+    initialProps: {
+      page: page,
+      pageSize: pageSize,
+    },
+  })
+
+  useEffect(() => {
+    queryResult.refetch()
+  }, [])
+
+  console.log(queryResult.data)
+
   return (
     <Box>
       <Typography>{TEXT.TEACHER}</Typography>
